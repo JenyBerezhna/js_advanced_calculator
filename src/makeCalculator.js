0977d6729fac5efaa68fa477be3adc361f6b2a5e
Jeny Berezhna <JenySmails24@gmail.com>
2025-03-18 20:09:01 +0000

    Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -7,12 +7,23 @@ function makeCalculator() {
   const calculator = {
     result: 0,
 
-    add: (a) => (b) => a + b,
-    subtract: (a) => (b) => a - b,
-    multiply: (a) => (b) => a * b,
-    divide: (a) => (b) => (b !== 0 ? a / b : 'Error: Division by zero'),
+    add(a) {
+      return (b) => a + b;
+    },
+    subtract(a) {
+      return (b) => a - b;
+    },
+    multiply(a) {
+      return (b) => a * b;
+    },
+    divide(a) {
+      return (b) => (b !== 0 ? a / b : 'Error: Division by zero');
+    },
 
     operate(callback, number) {
+      if (typeof callback !== 'function') {
+        throw new Error('Invalid callback: must be a function');
+      }
       this.result = callback(this.result)(number);
       return this;
     },
